32a6c84139eff8ebce494a364d9a93fa3cf8e08f
GRYE <yermolaev@outlook.com>
2018-08-22 15:46:34 +0300

    Types refactoring (#8)
    * Types refactoring
    
    * 0.8.3

--- src/main/scala/org/encryfoundation/common/Algos.scala
+++ src/main/scala/org/encryfoundation/common/Algos.scala
@@ -1,7 +1,8 @@
 package org.encryfoundation.common
 
 import java.nio.charset.Charset
-import scorex.crypto.authds.LeafData
+import org.encryfoundation.common.utils.TaggedTypes.LeafData
+import scorex.crypto.authds.{LeafData => ScorexLeaf}
 import scorex.crypto.authds.merkle.MerkleTree
 import scorex.crypto.encode.Base16
 import scorex.crypto.hash.{Blake2b256, Digest32}
@@ -22,7 +23,7 @@ object Algos {
   val charset: Charset = Charset.defaultCharset()
 
   def merkleTreeRoot(elements: Seq[LeafData]): Digest32 =
-    if (elements.isEmpty) emptyMerkleTreeRoot else MerkleTree(elements)(hash).rootHash
+    if (elements.isEmpty) emptyMerkleTreeRoot else MerkleTree(elements.map(ScorexLeaf @@ _.repr))(hash).rootHash
 
   lazy val emptyMerkleTreeRoot: Digest32 = Algos.hash(LeafData @@ Array[Byte]())
 }
